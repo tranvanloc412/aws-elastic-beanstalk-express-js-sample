a9ecd34d32b75db8d48584a30ff42059705794ed
Tran Van Loc <hoatuyethan_songvachet@yahoo.com>
2020-11-29 16:38:00 +0700

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('IT IS LOC!'));
+app.get('/', (req, res) => res.send('IT IS TEST~~'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
